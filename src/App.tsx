0e68303579c7ddebaae42a5b92d0038500b0df58
pushpak2000 <gitkeeper05@gmail.com>
2025-04-12 20:20:37 +0100

    fix of login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonSplitPane, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { useEffect, useState } from 'react';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Camera from './pages/Camera';
@@ -14,68 +15,97 @@ import {
   images,          
   journal,         
   map,   
-
 } from 'ionicons/icons';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
 setupIonicReact();
-{/** do not remove setupIonicReact its important so ionic things wont fall aparat */}
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = () => {
+      const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+      setIsAuthenticated(isLoggedIn);
+    };
+
+    checkAuth();
+    window.addEventListener('storage', checkAuth);
+    return () => window.removeEventListener('storage', checkAuth);
+  }, []);
 
   return (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-       
-        <Route  path="/login" component={Login} exact />
-        <Route path="/signup" component={SignUp} exact />
-        <Route path="/tabs" render={() => (
-          <IonSplitPane contentId="main">  {/**do not reverse order of ionsplitpane and routeroulet as first is parent */}
-             <Menu /> 
-             <IonRouterOutlet id="main">
-            <IonTabs>
-              <IonRouterOutlet>
-                <Route exact path="/tabs/home" component={Home} />
-                <Route exact path="/tabs/camera" component={Camera} />
-                <Route exact path="/tabs/journal" component={Journal} />
-                <Route exact path="/tabs/map" component={Map} />
-                <Redirect exact from="/tabs" to="/tabs/home" />
-              </IonRouterOutlet>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/login">
+            {isAuthenticated ? <Redirect to="/tabs/home" /> : <Login />}
+          </Route>
+          <Route exact path="/signup" component={SignUp} />
+          
+          <Route path="/tabs">
+            {!isAuthenticated ? <Redirect to="/login" /> : (
+              <IonSplitPane contentId="main">
+                <Menu />
+                <IonRouterOutlet id="main">
+                  <IonTabs>
+                    <IonRouterOutlet>
+                      <Route exact path="/tabs/home" component={Home} />
+                      <Route exact path="/tabs/camera" component={Camera} />
+                      <Route exact path="/tabs/journal" component={Journal} />
+                      <Route exact path="/tabs/map" component={Map} />
+                      <Redirect exact from="/tabs" to="/tabs/home" />
+                    </IonRouterOutlet>
 
-              <IonTabBar slot="bottom">
-                <IonTabButton tab="home" href="/tabs/home">
-                  <IonIcon icon={images} />
-                  <IonLabel>Feed</IonLabel>
-                </IonTabButton>
-                
-                <IonTabButton tab="camera" href="/tabs/camera">
-                  <IonIcon icon={camera} />
-                  <IonLabel>Camera</IonLabel>
-                </IonTabButton>
+                    <IonTabBar slot="bottom">
+                      <IonTabButton tab="home" href="/tabs/home">
+                        <IonIcon icon={images} />
+                        <IonLabel>Feed</IonLabel>
+                      </IonTabButton>
+                      
+                      <IonTabButton tab="camera" href="/tabs/camera">
+                        <IonIcon icon={camera} />
+                        <IonLabel>Camera</IonLabel>
+                      </IonTabButton>
 
-                <IonTabButton tab="journal" href="/tabs/journal">
-                  <IonIcon icon={journal} />
-                  <IonLabel>Journal</IonLabel>
-                </IonTabButton>
+                      <IonTabButton tab="journal" href="/tabs/journal">
+                        <IonIcon icon={journal} />
+                        <IonLabel>Journal</IonLabel>
+                      </IonTabButton>
 
-                <IonTabButton tab="map" href="/tabs/map">
-                      <IonIcon icon={map} />
-                      <IonLabel>Map</IonLabel>
-                    </IonTabButton>
+                      <IonTabButton tab="map" href="/tabs/map">
+                        <IonIcon icon={map} />
+                        <IonLabel>Map</IonLabel>
+                      </IonTabButton>
+                    </IonTabBar>
+                  </IonTabs>
+                </IonRouterOutlet>
+              </IonSplitPane>
+            )}
+          </Route>
 
-
-              </IonTabBar>
-            </IonTabs>
-            </IonRouterOutlet>
-            </IonSplitPane>
-          )} />
-
-          <Redirect exact from="/" to="/login" />
-
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-
-);
+          <Route exact path="/">
+            <Redirect to={isAuthenticated ? "/tabs/home" : "/login"} />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
 };
+
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,57 +1,89 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonList, IonItem, IonLabel } from '@ionic/react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import {
+  IonContent,
+  IonPage,
+  IonInput,
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardHeader,
+  IonCardTitle,
+} from '@ionic/react';
+
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import './Login.css';
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
+  const location = useLocation();
 
-const handleSubmit = () => {
-  if (username === "aviasnani" && password === "helloWorld"){
-    history.push('/tabs/home');
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username === "aviasnani" && password === "helloWorld") {
+      localStorage.setItem('isLoggedIn', 'true');
+      // Force a hard navigation to the home page
+      window.location.href = '/tabs/home';
+    } else {
+      alert("Invalid credentials");
+    }
+  };
 
-  }else{
-    alert("Invalid credentials");
-  }
-};
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow className="ion-justify-content-center ion-align-items-center" style={{ height: '100vh' }}>
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle className="ion-text-center">SpotImage Login</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <form onSubmit={handleSubmit}>
+                    <IonItem>
+                      <IonLabel position="floating">Username</IonLabel>
+                      <IonInput
+                        type="text"
+                        value={username}
+                        onIonChange={e => setUsername(e.detail.value!)}
+                        required
+                      />
+                    </IonItem>
 
-return (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Login</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent className="ion-padding">
-      <IonList>
-        <IonItem>
-        <IonLabel position="floating">Username</IonLabel>
-       <IonInput
-        value={username}
-        onIonChange={e => setUsername(e.detail.value!)}
-        clearInput
-      />
-      </IonItem>
-      <IonItem>
-      <IonLabel position="floating">Password</IonLabel>
-      <IonInput
-         type="password"
-         value={password}
-         onIonChange={e => setPassword(e.detail.value!)}
-         clearInput
-      />
-      </IonItem>
-      </IonList>
+                    <IonItem className="ion-margin-bottom">
+                      <IonLabel position="floating">Password</IonLabel>
+                      <IonInput
+                        type="password"
+                        value={password}
+                        onIonChange={e => setPassword(e.detail.value!)}
+                        required
+                      />
+                    </IonItem>
 
-      <div className="ion-padding">
-      <IonButton expand="full" onClick={handleSubmit}>
-        Login
-      </IonButton>
-      </div>
-    </IonContent>
-  </IonPage>
-);
+                    <IonButton expand="block" type="submit" className="ion-margin-top">
+                      Login
+                    </IonButton>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Login;
